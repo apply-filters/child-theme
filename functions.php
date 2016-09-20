c04236d0410e6cce366bd40a10fa8387f3b8fc5b
Pippin Williamson <pippin@pippinsplugins.com>
2016-09-20 10:12:27 -0500

    Register single sidebar

--- functions.php
+++ functions.php
@@ -1,5 +1,20 @@
 <?php
 
+function apply_filters_register_sidebars() {
+	register_sidebar(		
+		array(			
+			'id' => 'sidebar_single',			
+			'name' => __( 'Sidebar Single View', 'thstlang' ),			
+			'description' => __( 'This sidebar will appear on the single view of your blog.', 'thstlang' ),			
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',			
+			'after_widget' => '</div>',			
+			'before_title' => '<h3>',			
+			'after_title' => '</h3>'		
+		)
+	);	
+}
+add_action( 'widgets_init', 'apply_filters_register_sidebars' );
+
 function apply_filters_theme_init() {
 	global $apply_filters_theme;
 	$dir_path = dirname( __FILE__ );
